eab138e30362c10858104a9b0a55107fcf5e1933
vivien-jiang <theuservivien@gmail.com>
2026-05-11 17:15:26 -0400

    editing kidney tutorial

--- inst/scripts/visiumKidneySpatialCorrelation.R
+++ inst/scripts/visiumKidneySpatialCorrelation.R
@@ -10,12 +10,11 @@ library(Matrix)
 library(BiocGenerics)
 
 
-# The initlization is the same as the tutorial 
-# TODO: add tutorial link 
+# The initlization is the same as the tutorial
 
 # The Visium mouse kidney datasets can be found Zenodo: https://doi.org/10.5281/zenodo.17676991
-# IL3 - ischemia acute kidney injury dataset 
-# NL3 - control dataset 
+# IL3 - ischemia acute kidney injury dataset
+# NL3 - control dataset
 
 # AKI counts ###################################################################
 zenodo_url <- "https://zenodo.org/records/19074288/files/IL3_filtered_feature_bc_matrix.h5?download=1"
@@ -25,7 +24,7 @@ download.file(zenodo_url, temp_h5, mode = "wb")
 
 # Read the CSC-encoded matrix components from the HDF5 file
 file_barcodes <- as.character(rhdf5::h5read(temp_h5, "matrix/barcodes"))
-barcodes_matrix <- rhdf5::h5read(temp_h5, "matrix") # stores the sparse matrix components 
+barcodes_matrix <- rhdf5::h5read(temp_h5, "matrix") # stores the sparse matrix components
 
 # convert CSC encoding into an R dgCMatrix
 aki_counts <- Matrix::sparseMatrix(
@@ -47,7 +46,7 @@ download.file(zenodo_url, temp_h5, mode = "wb")
 
 # Read the CSC-encoded matrix components from the HDF5 file
 file_barcodes <- as.character(rhdf5::h5read(temp_h5, "matrix/barcodes"))
-barcodes_matrix <- rhdf5::h5read(temp_h5, "matrix") # stores the sparse matrix components 
+barcodes_matrix <- rhdf5::h5read(temp_h5, "matrix") # stores the sparse matrix components
 
 # convert CSC encoding into an R dgCMatrix
 control_counts <- Matrix::sparseMatrix(
@@ -88,8 +87,6 @@ colnames(aki_pos) <- c("x", "y")
 ctrl_pos$group <- "Control"
 aki_pos$group  <- "AKI"
 
-# TODO: error in this line 
-zenodo_url <- "https://zenodo.org/records/19486091/files/aki_region_onehot_STalign_to_ctrl_region_onehot_affine_only.csv.gz?download=1"
 zenodo_url <- "https://zenodo.org/records/19486091/files/aki_region_onehot_STalign_to_ctrl_region_onehot_affine_only.csv.gz?download=1"
 aki_STalign <- read.csv(gzcon(url(zenodo_url, open = "rb"), text = TRUE), header = TRUE, row.names =1)
 
@@ -99,7 +96,7 @@ aki_pos_aligned <- aki_STalign[,c("aligned_x", "aligned_y")]
 #rename columns and add group for consistency with ctrl_pos_rot
 colnames(aki_pos_aligned) <- c("x", "y")
 aki_pos_aligned$group  <- "AKI"
-# rotate the tissue by 90-degrees for consistency with the paper 
+# rotate the tissue by 90-degrees for consistency with the paper
 aki_pos_rot <- transform(aki_pos_aligned, x = y, y = -x + max(aki_pos_aligned$x))
 aki_pos_aligned <- aki_pos_rot
 
@@ -115,7 +112,6 @@ AKI_aki_SE <- SpatialExperiment::SpatialExperiment(
   assays = list(counts = aki_counts),
   spatialCoords = as.matrix(aki_pos_aligned[,1:2])
 )
-
 
 input <- list(AKI_ctrl = AKI_ctrl_SE, AKI_aki = AKI_aki_SE)
 rast <- SEraster::rasterizeGeneExpression(
@@ -135,58 +131,34 @@ assay(rast$AKI_aki, "CPM") <- Matrix::t(Matrix::t(assay(rast$AKI_aki))/Matrix::c
 w <- MERINGUE::getSpatialNeighbors(SpatialExperiment::spatialCoords(rast$AKI_ctrl), filterDist = 10)
 
 moransI <- function(SE_temp, filterDist, assayName=1){
-  
-  # use MERINGUE to get the neighbor-relationships 
+
+  # use MERINGUE to get the neighbor-relationships
   w <- MERINGUE::getSpatialNeighbors(SpatialExperiment::spatialCoords(SE_temp), filterDist = filterDist)
   par(mfrow=c(1,1))
   MERINGUE::plotNetwork(SpatialExperiment::spatialCoords(SE_temp), w)
-  
+
   # Identify significantly spatially auto-correlated genes
   I <- MERINGUE::getSpatialPatterns(SummarizedExperiment::assays(SE_temp)[[assayName]], w)
-  
+
   return(I)
 }
 
 moransI_ctrl <- moransI(rast$AKI_ctrl, filterDist = 10, assayName = "CPM")
-
 moransI_aki <- moransI(rast$AKI_aki, filterDist = 10, assayName = "CPM")
 
 svg_ctrl <- moransI_ctrl %>% filter(p.adj == 0) %>% rownames()
 svg_aki <- moransI_aki %>% filter(p.adj == 0) %>% rownames()
 
-svg_int <- intersect(svg_ctrl, svg_aki) # genes that are SVGs in both conditions 
+svg_int <- intersect(svg_ctrl, svg_aki) # genes that are SVGs in both conditions
 
-
-good.genes <- names(which(
-  (Matrix::rowSums(SummarizedExperiment::assay(rast$AKI_ctrl, 'CPM') > 0) / ncol(SummarizedExperiment::assay(rast$AKI_ctrl, 'CPM'))*100 > 5) &
-  (Matrix::rowSums(SummarizedExperiment::assay(rast$AKI_aki, 'CPM') > 0) / ncol(SummarizedExperiment::assay(rast$AKI_aki, 'CPM'))*100 > 5)))
-
-# TODO: there are duplicates in rast for some reason 
-
-# x <- setdiff(rownames(moransI_ctrl), make.names(rownames(rast$AKI_ctrl)))
-# length(x)
-
-# y <- setdiff(make.names(rownames(rast$AKI_ctrl)), rownames(moransI_ctrl))
-# length(y)
-
-# # set up parameters for spatialCorrelationGeneExp 
-# rownames(rast$AKI_ctrl)[duplicated(rownames(rast$AKI_ctrl))]
-# # sum(duplicated(rownames(rast$AKI_ctrl)))
-
-# rownames(rast$AKI_aki)[duplicated(rownames(rast$AKI_aki))]
-# # sum(duplicated(rownames(rast$AKI_aki)))
-
-# # choosing the genes that are SVGs in both conditions 
+# choosing the genes that are SVGs in both conditions
 genes_chosen <- svg_int
 
-# TODO: the work around 
+# alternative: choosing the genes that are expressed in greater than 5% of all spots
+# genes_chosen <- good.genes
 
 rownames(rast$AKI_ctrl) <- make.names(rownames(rast$AKI_ctrl), unique = TRUE)
 rownames(rast$AKI_aki)  <- make.names(rownames(rast$AKI_aki),  unique = TRUE)
-
-
-## alternative: choosing the genes that are expressed in greater than 5% of all spots
-# genes_chosen <- good.genes
 
 # The input is the spatial experiments subset to the chosen genes of interest
 input <- list('AKI_ctrl'= rast$AKI_ctrl[genes_chosen,],
@@ -198,24 +170,21 @@ deltaList <- replicate(length(genes_chosen), c(0.01, 0.05, seq(0.1, 0.9, .1)), s
 # creates 100 permutations and then if a gene has an empirical p-value is less than 0.05, creates 1000 permutations for more accurate p-value estimation
 nPermutations <- c(100, 1000)
 
-# running spatialCorrelationGeneExp 
+# running spatialCorrelationGeneExp
 start_time <- Sys.time()
 deltaList <- replicate(length(genes_chosen), c(0.01, 0.05, seq(0.1, 0.9, .1)), simplify = FALSE)
 
-# TODO: remove devtools
-devtools::load_all()
-
 kidneyCorrelation <- spatialCorrelationGeneExpIterPermutations(
-  input, 
-  nPermutations = nPermutations, 
-  deltaX = deltaList, 
-  deltaY = deltaList, 
+  input,
+  nPermutations = nPermutations,
+  deltaX = deltaList,
+  deltaY = deltaList,
   assayName = "CPM", # use the CPM normalized assay for this analysis
   nThreads = 22, # parallelize genes across threads
   BPPARAM = BiocParallel::MulticoreParam(), #setting up parallel-execution back-end
   verbose = TRUE, # prints name of gene as progress updates, which is helpful for long runs
   seed = 0) # set seed for reproducibility of the permutation results
 end_time <- Sys.time()
-print(end_time - start_time) 
+print(end_time - start_time)
 
 save(kidneyCorrelation, file = file.path("inst", "extdata", "kidneyCorrelation.RData"))
